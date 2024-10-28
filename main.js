6e64b15d934d072ec226f0bc3883119fb1d158af
Denjix7 <denzhizhko@yandex.ru>
2024-10-28 12:18:54 +0300

    исправил погрешности

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let burger = document.querySelector("#burger");
 let menu = document.querySelector("#menu");
+let close=document.querySelector("#close")
 
 burger.addEventListener("click",()=>{
-    if (menu.classList.contains(".header-burger__none") ==true) {
-       menu.classList.remove(".header-burger__none");
+    if (menu.classList.contains("sub-menu-none") ==true) {
+       menu.classList.remove("sub-menu-none");
     } else{
-        menu.classList.add(".header-burger__none");
+        menu.classList.add("sub-menu-none");
+    }
+})
+
+close.addEventListener("click",()=>{
+    if (menu.classList.contains("sub-menu-none") ==true) {
+       menu.classList.remove("sub-menu-none");
+    } else{
+        menu.classList.add("sub-menu-none");
     }
 })
